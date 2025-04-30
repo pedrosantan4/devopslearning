8e8d4b0875f5543fe5cf2e7c3dee538f248d914f
Pedro <ap911152@gmail.com>
2025-04-30 00:47:21 -0300

    teste 3

--- app/app.py
+++ app/app.py
@@ -15,8 +15,15 @@ s3 = boto3.client(
 def home():
     return "API online!"
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["file"]
     s3.upload_fileobj(file, "meu-bucket", file.filename)
     return f"Arquivo {file.filename} enviado com sucesso!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
